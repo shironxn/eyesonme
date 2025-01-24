7eeb6dabe17ad40661e58389f7750913922bfc27
shironxn <shironxn@gmail.com>
2025-01-25 01:00:24 +0700

    feat: add animation scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/nav/navbar";
 import { Footer } from "@/components/footer";
+import AOSComponent from "@/hooks/aos";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        {children}
-        <Footer />
+        <AOSComponent>
+          <Navbar />
+          {children}
+          <Footer />
+        </AOSComponent>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import * as motion from "motion/react-client";
 
 const welcomes = [
   "welcome",
@@ -79,7 +80,10 @@ export default function Home() {
     <>
       {/* Hero Section */}
       <div className="py-12 md:py-24 bg-bg">
-        <div className="container mx-auto flex flex-col-reverse lg:flex-row justify-between items-center gap-8 md:gap-16">
+        <div
+          className="container mx-auto flex flex-col-reverse lg:flex-row justify-between items-center gap-8 md:gap-16"
+          data-aos="zoom-out"
+        >
           <div className="space-y-6 text-center lg:text-left">
             <div className="space-y-4 text-main">
               <h1>Harsanala Xayasadha</h1>
@@ -105,7 +109,9 @@ export default function Home() {
       </div>
 
       {/* Marquee Section */}
-      <Marquee items={welcomes} />
+      <div>
+        <Marquee items={welcomes} />
+      </div>
 
       {/* Tentang Kami Section */}
       <div
@@ -114,7 +120,10 @@ export default function Home() {
   bg-[size:70px_70px] pb-12 border-b-2 border-black md:gap-16 py-12 md:py-24"
       >
         <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-12">
-          <div className="flex flex-col justify-center text-center md:items-start md:text-left">
+          <div
+            className="flex flex-col justify-center text-center md:items-start md:text-left"
+            data-aos="fade-up"
+          >
             <h2>Tentang Kami</h2>
             <p>
               OSIS SMAN 48 Jakarta adalah wadah bagi siswa untuk mengembangkan
@@ -123,7 +132,10 @@ export default function Home() {
             </p>
           </div>
           <div className="space-y-4 md:space-y-8">
-            <div className="shadow-accent shadow-[4px_6px_0px_#000] rounded-base border-2 border-accent">
+            <div
+              className="shadow-accent shadow-[4px_6px_0px_#000] rounded-base border-2 border-accent"
+              data-aos="fade-up"
+            >
               <Image
                 src={"/image1.jpg"}
                 alt="image"
@@ -134,7 +146,10 @@ export default function Home() {
             </div>
 
             <div className="relative">
-              <div className="shadow-accent shadow-[4px_6px_0px_#000] rounded-base border-2 border-accent">
+              <div
+                className="shadow-accent shadow-[4px_6px_0px_#000] rounded-base border-2 border-accent"
+                data-aos="fade-up"
+              >
                 <Image
                   src={"/image2.jpg"}
                   alt="image"
@@ -157,32 +172,52 @@ export default function Home() {
       </div>
 
       {/* Visi Misi Section */}
-      <div className="py-12 md:py-24 bg-secondary rounded-b-[48px] md:rounded-b-[96px] relative">
+      <div
+        className="py-12 md:py-24 bg-secondary rounded-b-[48px] md:rounded-b-[96px] relative"
+        id="visi-misi"
+      >
         <div className="container mx-auto space-y-12 z-10 relative">
-          <div className="flex flex-col-reverse lg:flex-row gap-4 md:gap-12 items-center">
+          <motion.div
+            className="flex flex-col-reverse md:flex-row gap-4 md:gap-12 items-center"
+            data-aos="fade-up"
+            whileHover={{
+              scale: 1.1,
+            }}
+            transition={{ duration: 0.2 }}
+          >
             <div className="rounded-base border-border border-2 shadow-shadow p-6 bg-bg w-full">
               <p>{visi}</p>
             </div>
             <h2>Visi</h2>
-          </div>
+          </motion.div>
 
-          <div className="flex flex-col lg:flex-row gap-4 md:gap-12 items-center">
+          <motion.div
+            className="flex flex-col md:flex-row gap-4 md:gap-12 items-center"
+            data-aos="fade-up"
+            whileHover={{
+              scale: 1.1,
+            }}
+            transition={{ duration: 0.2 }}
+          >
             <h2>Misi</h2>
             <div className="rounded-base border-border border-2 shadow-shadow p-6 bg-bg w-full">
               <ol className="list-decimal list-inside space-y-2">
                 {misi.map((item, index) => (
-                  <li key={index}>{item}</li>
+                  <li key={index} className="gap-3 flex items-start ">
+                    <span>{index + 1}.</span>
+                    {item}
+                  </li>
                 ))}
               </ol>
             </div>
-          </div>
+          </motion.div>
         </div>
         <Image
           src={"/shape-1.svg"}
           alt="shape"
           width={200}
           height={200}
-          className="absolute top-10 left-0 -translate-x-16 lg:top-16 lg:-translate-x-10 xl:translate-x-0"
+          className="hidden lg:block absolute top-16 -translate-x-12 xl:-translate-x-8"
         />
         <Image
           src={"/shape-2.svg"}
@@ -203,18 +238,25 @@ export default function Home() {
       {/* Berita Terbaru */}
       <div className="py-12 md:py-24">
         <div className="container mx-auto space-y-12">
-          <div className="text-center">
+          <div className="text-center" data-aos="fade-up">
             <h2>Berita Terbaru</h2>
             <p>
               Berita terkini seputar kegiatan, prestasi, dan acara OSIS SMAN 48
               Jakarta.
             </p>
           </div>
-          <div className="flex overflow-x-scroll snap-x snap-mandatory flex-nowrap md:grid md:grid-cols-3 md:overflow-visible gap-8 no-scrollbar">
+          <div
+            className="flex overflow-x-scroll snap-x snap-mandatory flex-nowrap md:grid md:grid-cols-3 md:overflow-visible gap-8 no-scrollbar"
+            data-aos="fade-up"
+          >
             {berita.map((item, index) => (
-              <div
+              <motion.div
                 key={index}
                 className="snap-center shrink-0 w-full md:w-auto p-2"
+                whileHover={{
+                  scale: 1.1,
+                }}
+                transition={{ duration: 0.3 }}
               >
                 <Card>
                   <CardHeader>
@@ -233,7 +275,7 @@ export default function Home() {
                     <CardDescription>{item.description}</CardDescription>
                   </CardHeader>
                 </Card>
-              </div>
+              </motion.div>
             ))}
           </div>
         </div>
@@ -243,14 +285,14 @@ export default function Home() {
       <div className="py-12 md:py-24 bg-secondary rounded-t-[48px] md:rounded-t-[96px]">
         <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 items-center">
           <div className="space-y-8">
-            <div className="text-center md:text-left">
+            <div className="text-center md:text-left" data-aos="fade-up">
               <h2>Aspirasi</h2>
               <p>Sampaikan aspirasi Anda untuk membantu kami.</p>
             </div>
-            <div className="flex justify-center md:hidden">
+            <div className="flex justify-center md:hidden" data-aos="fade-up">
               <Image src="/berita.png" alt="berita" width={200} height={200} />
             </div>
-            <div className="space-y-4">
+            <div className="space-y-4" data-aos="fade-up">
               <Label htmlFor="aspirasi">Tulis aspirasi Anda</Label>
               <Textarea
                 id="aspirasi"
@@ -260,7 +302,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="hidden md:flex justify-center">
+          <div className="hidden md:flex justify-center" data-aos="fade-up">
             <Image src="/berita.png" alt="berita" width={250} height={250} />
           </div>
         </div>
