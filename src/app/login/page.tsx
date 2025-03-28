bfdc348832c2fdc0bdca20825ab6b1d3f91c79aa
shironxn <shironxn@gmail.com>
2025-03-28 15:02:10 +0700

    chore: update login ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,20 +6,36 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="container flex items-center justify-center min-h-screen px-4 py-8">
-      <Card className="w-full max-w-md md:p-6">
-        <CardHeader>
-          <CardTitle>Masuk</CardTitle>
-          <CardDescription>
-            Untuk masuk ke halaman ini, Anda harus login dulu.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <LoginForm />
-        </CardContent>
+    <div className="container flex items-center justify-center min-h-screen py-12 md:py-24">
+      <Card className="w-full max-w-4xl overflow-hidden">
+        <div className="flex flex-col md:flex-row">
+          <div className="flex items-center justify-center p-8 md:w-2/5 bg-bw">
+            <div className="relative w-64 h-64">
+              <Image
+                src="/login/lock.svg"
+                alt="Lock"
+                fill
+                className="object-contain"
+                priority
+              />
+            </div>
+          </div>
+          <div className="md:w-3/5 p-2">
+            <CardHeader>
+              <CardTitle className="text-3xl">Welcome!</CardTitle>
+              <CardDescription className="text-base text-muted">
+                Silahkan login menggunakan akun yang kamu miliki.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <LoginForm />
+            </CardContent>
+          </div>
+        </div>
       </Card>
     </div>
   );
--- src/components/login/form.tsx
+++ src/components/login/form.tsx
@@ -4,58 +4,64 @@ import { Login } from "@/app/actions/auth";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useToast } from "@/hooks/use-toast";
-import { LockKeyhole, User } from "lucide-react";
+import { Loader2Icon, LockKeyholeIcon, UserIcon } from "lucide-react";
+import { useTransition } from "react";
 
 export function LoginForm() {
   const { toast } = useToast();
+  const [isPending, startTransition] = useTransition();
 
   const handleLogin = async (formData: FormData) => {
-    const res = await Login(formData);
+    startTransition(async () => {
+      const res = await Login(formData);
 
-    if (res) {
-      toast({
-        title: res.message,
-        description: res.details,
-        variant: "destructive",
-      });
-    }
+      if (res) {
+        toast({
+          title: res.message,
+          description: res.details,
+          variant: "destructive",
+        });
+      }
+    });
   };
 
   return (
-    <form action={handleLogin} className="space-y-4">
-      <div className="space-y-2">
-        <div className="relative">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-muted-foreground">
-            <User size={18} />
+    <form action={handleLogin} className="space-y-6">
+      <div className="space-y-4">
+        <div className="space-y-2">
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <UserIcon size={18} />
+            </div>
+            <Input
+              id="username"
+              name="username"
+              placeholder="Username"
+              className="pl-10"
+              required
+            />
           </div>
-          <Input
-            id="username"
-            name="username"
-            placeholder="Username"
-            className="pl-10"
-            required
-          />
+        </div>
+
+        <div className="space-y-2">
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <LockKeyholeIcon size={18} />
+            </div>
+            <Input
+              id="password"
+              name="password"
+              placeholder="Password"
+              type="password"
+              className="pl-10"
+              required
+            />
+          </div>
         </div>
       </div>
 
-      <div className="space-y-2">
-        <div className="relative">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none text-muted-foreground">
-            <LockKeyhole size={18} />
-          </div>
-          <Input
-            id="password"
-            name="password"
-            placeholder="Password"
-            type="password"
-            className="pl-10"
-            required
-          />
-        </div>
-      </div>
-
-      <Button className="w-full mt-6 bg-main hover:bg-main/90" type="submit">
-        Masuk
+      <Button size="lg" type="submit" disabled={isPending}>
+        {isPending && <Loader2Icon className="animate-spin mr-2" />} Log In
       </Button>
     </form>
   );
